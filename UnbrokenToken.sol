2b7cb7a4a4d521e216162f63a38191dcdc2e332d
jasonbucks <102486437+jasonbucks@users.noreply.github.com>
2022-10-22 07:14:18 -0700

    Update UnbrokenToken.sol
    fixed typo

--- UnbrokenToken.sol
+++ UnbrokenToken.sol
@@ -10,7 +10,7 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
-// Create a constructor for the KaseiCoin contract and have the contract inherit the libraries that you imported from OpenZeppelin.
+// Create a constructor for the UnbrokenToken contract and have the contract inherit the libraries that you imported from OpenZeppelin.
 contract UnbrokenToken is ERC20, ERC20Detailed, ERC20Mintable {
     constructor(
         string memory name,
