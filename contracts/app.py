0521be5644b74503e480517da5b1ef8da447ae5a
Steven Shelton <steve@rsvpresea.com>
2022-11-02 18:08:53 -0700

    Compiled UBT and UBCrowdsale in Remix. Copied two different Abi's one from UBT and UBCrowdsale. I then saved those abi's to vscode and was able to code some functions and most of the calls from both smart contracts

--- contracts/app.py
+++ contracts/app.py
@@ -5,7 +5,8 @@ from web3 import Web3
 from pathlib import Path
 from dotenv import load_dotenv
 import streamlit as st
-
+import time
+from PIL import Image
 load_dotenv()
 
 #Define and connect a WEB3 provider
@@ -21,8 +22,8 @@ st.cache(allow_output_mutation=True)
 def load_contract():
 
     # Load the contract ABI
-    with open(Path('./contracts/compiled/ubt_abi.json')) as f:
-        ubt_abi = json.load(f)
+    with open(Path("C:\\Users\\steve\\Desktop\\unbrokenToken\\unbrokenToken\\contracts\\compiled\\ubtoken_abi.json")) as f:
+        ubtoken_abi = json.load(f)
 
     # Set the contract address (this is the address of the deployed contract)
     contract_address = os.getenv("SMART_CONTRACT_ADDRESS")
@@ -30,8 +31,97 @@ def load_contract():
     # Get the contract
     contract = w3.eth.contract(
         address=contract_address,
-        abi=ubt_abi
+        abi= ubtoken_abi
     )
 
     return contract
 
+# Load the contract
+contract = load_contract()
+
+################################################################################
+# Register New Artwork
+################################################################################
+
+#Tried uploading introductry video to streamlit.
+#st.video("https:\\www.canva.com\\design/DAFQpjffewI\\kocMw1yT_vCC98hKxFOUng/watch?utm_content=DAFQpjffewI&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink")
+
+#Tried uploading unbroken image.
+#unbroken_logo= Image.open('images\\ubtlogo.png')
+
+st.sidebar.selectbox("UBT Crowdsale", ["Learn more", "Buy Tokens"])
+
+
+video_file = open('ubtkenvid(2).mp4', 'rb')
+video_bytes = video_file.read()
+
+st.video(video_bytes)
+
+font="monospace"
+
+st.title("Unbroken Token")
+accounts = w3.eth.accounts
+
+address = st.selectbox("Select Owner Address", options=accounts)
+token_uri= st.text_input("Token URI")
+
+# Fucntions
+if st.button("Register Account"):
+
+    tx_hash = contract.functions.registerAddress(address, token_uri).transact({'from': address, 'gas': 1000000})
+
+    receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+
+    st.write("Transaction receipt mined:")
+
+    st.write(dict(receipt))
+
+st.markdown("---")
+
+token_id = int(st.text_input("Token ID (uint256)",value=0))
+
+if st.button("Approve"):
+
+    tx_hash = contract.functions.approve(address, token_id).transact({'from': address, 'gas': 1000000})
+    receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+    st.write("Transaction receipt mined:")
+    st.write(dict(receipt))
+
+st.markdown("---")
+
+boolean = bool(st.text_input("Boolean",value=True))
+
+if st.button("Set Approval for All"):
+
+    tx_hash = contract.functions.setApprovalForAll(address, boolean).transact({'from': address, 'gas': 1000000})
+    receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+    st.write("Transaction receipt mined:")
+    st.write(dict(receipt))
+
+st.markdown("---")
+
+address1 = st.selectbox("Select Owner Address 1", options=accounts)
+address2 = st.selectbox("Select Owner Address 2 ", options=accounts)
+token_id = int(st.text_input("Token ID (uint256) ",value=0))
+
+if st.button("Safe Transfer From"):
+
+    tx_hash = contract.functions.safeTransferFrom(address1,address2,token_id).transact({'from': address, 'gas': 1000000})
+    receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+    st.write("Transaction receipt mined:")
+    st.write(dict(receipt))
+
+st.markdown("---")
+
+#boolean = bool(st.text_input("Boolean",value=True))
+
+##address2 = st.selectbox("Select Owner Address 2", options=accounts)
+
+#if st.button("Approval For All"):
+
+    #tx_hash = contract.functions.setApprovalForAll(address,boolean).transact({'from': address, 'gas': 1000000})
+    #receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+    #st.write("Transaction receipt mined:")
+    #st.write(dict(receipt))
+
+#st.markdown("---")
